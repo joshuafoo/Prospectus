5e37ae3f545fcf7e1fd07956d27e710324a9964b
Lucas Lim <lucas_lim_kai_gee@s2017.sst.edu.sg>
2018-07-16 21:46:33 +0800

    Changed Storyboard added images.

---
+++ Prospectus App/MainCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MainCollectionViewCell.swift
+//  Prospectus App
+//
+//  Created by Lucas Lim Kai Gee on 16/7/18.
+//  Copyright © 2018 E-Rong Yong. All rights reserved.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imgImage: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+}
